0538afa4af8ba9bdcd4f06bd0b34a5bba3953a38
Denis <chugunov-official@mail.ru>
2019-11-04 18:51:07 +0300

    Change lexer and fill parser

--- pymilldb/lexer.py
+++ pymilldb/lexer.py
@@ -1,6 +1,6 @@
 import re
 
-END = '\000'
+END_CHAR = '\000'
 
 SYMBOLS = {
     "(": "LPARENT",
@@ -66,14 +66,14 @@ WHITESPACE = ' \t\n'
 
 class Pos(object):
     def __init__(self, program: str):
-        self.program = COMMENT.sub('', program)  # Чистим комментарии
+        self.program = program  # Чистим комментарии
         self.pos = 0
         self.col = 1
         self.line = 1
 
     @property
     def char(self):
-        return self.program[self.pos] if self.pos == len(self.program) else END
+        return self.program[self.pos] if self.pos == len(self.program) else END_CHAR
 
     def next(self):
         if self.char == '\n':
@@ -83,6 +83,18 @@ class Pos(object):
             self.col += 1
         self.pos = self.pos + 1 if self.pos < len(self.program) else self.pos
 
+    def isdecimal(self):
+        return self.char.isdecimal()
+
+    def islower(self):
+        return self.char.islower()
+
+    def isupper(self):
+        return self.char.isupper()
+
+    def isspace(self):
+        return self.char.isspace()
+
     def __str__(self):
         return "<{}:{}>".format(self.line, self.col)
 
@@ -90,52 +102,73 @@ class Pos(object):
         return "<{}:{}> - <{}:{}>".format(other.line, other.pos, self.line, self.pos)
 
 
-def lexer(p: Pos):
-    while p.char != END:
-        while p.char in WHITESPACE:
-            p.next()
+class Lexer(object):
 
-        if p.char in "();,=":
-            yield 'SYMBOLS', SYMBOLS[p.char], p.char
-            p.next()
-            continue
+    def __init__(self, text):
+        self.orig_text = text
+        self.text = COMMENT.sub('', text)  # Чистим комментарии
+        self.pos = Pos(self.text)
+        self.__gen_lex = self.__lex()
+        self.cur_token, self.cur_value, self.cur_raw_value = next(self.__gen_lex)
 
-        elif p.char in "<>":
-            c1 = p.char
-            p.next()
-            c2 = c1 + p.char
-            if c2 in ("<>", "<=", ">="):
-                yield 'SYMBOLS', SYMBOLS[c2], c2
-                p.next()
-            else:
-                yield 'SYMBOLS', SYMBOLS[c1], c1
-            continue
+    def next(self):
+        self.cur_token, self.cur_value, self.cur_raw_value = next(self.__gen_lex)
 
-        if '1' <= p.char <= '9':
-            buf = ''
-            while '0' <= p.char <= '9':
-                buf += p.char
-                p.next()
-            yield 'INTEGER', int(buf), buf
-            continue
-        is_param = p.char == '@' and (p.next() or True)
-        if 'a' <= p.char <= 'z' or 'A' <= p.char <= 'Z':
-            buf = ''
-            while 'a' <= p.char <= 'z' or 'A' <= p.char <= 'Z' or '0' <= p.char <= '9' or p.char == '_':
-                buf += p.char
-                p.next()
-            if is_param:
-                yield 'PARAMETER', buf, '@'+buf
-            else:
-                keyword = KEYWORDS.get(buf.lower())
-                kind = TYPES.get(buf.lower()) if not keyword else None
-                yield (
-                    ('KEYWORD', keyword, buf)
-                    if keyword else
-                    ('TYPE', kind, buf)
-                    if kind else
-                    ('IDENTIFIER', buf, buf)
-                )
-            continue
-        #  todo: Exception wrong symbol
+    def __lex(self):
+        while self.pos.char != END_CHAR:
+            # Пропускаем пробелы
+            while self.pos.isspace():
+                self.pos.next()
 
+            # Проверка на спецсимвол длины 1
+            if self.pos.char in "();,=":
+                yield 'SYMBOLS', SYMBOLS[self.pos.char], self.pos.char
+                self.pos.next()
+                continue
+            # Проверка на спецсимвол длины 2
+            if self.pos.char in "<>":
+                c1 = self.pos.char
+                self.pos.next()
+                c2 = c1 + self.pos.char
+                if c2 in ("<>", "<=", ">="):
+                    yield 'SYMBOLS', SYMBOLS[c2], c2
+                    self.pos.next()
+                else:
+                    yield 'SYMBOLS', SYMBOLS[c1], c1
+                continue
+            # Проверка на число
+            if self.pos.isdecimal():
+                buf = ''
+                while self.pos.isdecimal():
+                    buf += self.pos.char
+                    self.pos.next()
+                yield 'INTEGER', int(buf), buf
+                continue
+            # Проверка на параметр/идентификатор
+            is_param = self.pos.char == '@' and (self.pos.next() or True)
+            if self.pos.islower() or self.pos.isupper():
+                buf = ''
+                while (
+                        self.pos.islower() or
+                        self.pos.isupper() or
+                        self.pos.isdecimal() or
+                        self.pos.char == '_'
+                ):
+                    buf += self.pos.char
+                    self.pos.next()
+                if is_param:
+                    yield 'PARAMETER', buf, '@'+buf
+                else:
+                    keyword = KEYWORDS.get(buf.lower())
+                    kind = TYPES.get(buf.lower()) if not keyword else None
+                    yield (
+                        ('KEYWORD', keyword, buf)
+                        if keyword else
+                        ('TYPE', kind, buf)
+                        if kind else
+                        ('IDENTIFIER', buf, buf)
+                    )
+                continue
+            #  todo: Exception processing
+        while True:
+            yield 'END', END_CHAR, END_CHAR
--- pymilldb/parser.py
+++ pymilldb/parser.py
@@ -1,138 +1,241 @@
-def program(lex):
-    # <program_element_list>
-    pass
+from .lexer import Lexer
 
 
-def program_element_list(lex):
-    # <program_element>
-    # <program_element_list> <program_element>
-    pass
+class Token(Lexer):
+    def __eq__(self, other):
+        return self.cur_token == other
+
+    def __rshift__(self, other):
+        if self.cur_token == other:
+            val = self.cur_value
+            self.next()
+            return val
+        else:
+            raise Exception  # todo: Обработка ошибок
 
 
-def program_element(lex):
-    # <table_declaration>
-    # <procedure_declaration>
-    # <sequence_declaration>
-    pass
+class Parser(object):
 
+    def __init__(self, token: Token):
+        self.token = token
 
-def table_declaration(lex):
-    # CREATE TABLE id LPARENT <column_declaration_list> RPARENT SEMICOLON
-    pass
+    def program(self):
+        # <program_element_list>
+        self.program_element_list()
 
+    def program_element_list(self):
+        # <program_element>
+        # <program_element_list> <program_element>
+        self.program_element()
+        if self.token != 'END':
+            self.program_element_list()
 
-def column_declaration_list(lex):
-    # id type
-    # id type PK
-    pass
+    def program_element(self):
+        # <table_declaration>
+        # <procedure_declaration>
+        # <sequence_declaration>
+        self.token >> 'CREATE'
+        if self.token == 'TABLE':
+            self.table_declaration()
+        elif self.token == 'PROCEDURE':
+            self.procedure_declaration()
+        elif self.token == 'SEQUENCE':
+            self.sequence_declaration()
+        raise Exception  # todo
 
+    def table_declaration(self):
+        # CREATE TABLE id LPARENT <column_declaration_list> RPARENT SEMICOLON
+        self.token.next()  # self.token >> 'TABLE'
+        table_name = self.token >> 'IDENTIFIER'
+        self.token >> 'LPARENT'
+        self.column_declaration_list()
+        self.token >> 'RPARENT'
+        self.token >> 'SEMICOLON'
 
-def procedure_declaration(lex):
-    # CREATE PROCEDURE id LPARENT <parameter_declaration_list> RPARENT BEGIN <statement_list> END SEMICOLON
-    pass
+    def column_declaration_list(self):
+        # column_declaration
+        # column_declaration_list COMMA column_declaration
+        self.column_declaration()
+        if self.token == 'COMMA':
+            self.token.next()  # self.token >> 'COMMA'
+            self.column_declaration_list()
 
+    def column_declaration(self):
+        # id type
+        # id type PK
+        column_name = self.token >> 'IDENTIFIER'
+        column_type = self.token >> 'TYPE'
+        if self.token == 'PK':
+            self.token.next()  # self.token >> 'PK'
 
-def parameter_declaration_list(lex):
-    # <parameter_declaration>
-    # <parameter_declaration_list> COMMA <parameter_declaration>
-    pass
+    def procedure_declaration(self):
+        # CREATE PROCEDURE id LPARENT <parameter_declaration_list> RPARENT BEGIN <statement_list> END SEMICOLON
+        self.token.next()  # self.token >> 'PROCEDURE'
+        procedure_name = self.token >> 'IDENTIFIER'
+        self.parameter_declaration_list()
+        self.token >> 'RPARENT'
+        self.token >> 'BEGIN'
+        self.statement_list()
+        self.token >> 'END'
+        self.token >> 'SEMICOLON'
 
+    def parameter_declaration_list(self):
+        # <parameter_declaration>
+        # <parameter_declaration_list> COMMA <parameter_declaration>
+        self.parameter_declaration()
+        if self.token == 'COMMA':
+            self.token.next()  # self.token >> 'COMMA'
+            self.parameter_declaration_list()
 
-def parameter_declaration(lex):
-    # pid type <parameter_mode>
-    pass
+    def parameter_declaration(self):
+        # pid type <parameter_mode>
+        parameter_name = self.token >> 'PARAMETER'
+        parameter_type = self.token >> 'TYPE'
+        self.parameter_mode()
 
+    def parameter_mode(self):
+        # IN
+        # OUT
+        if self.token == 'IN':
+            self.token.next()  # self.token >> 'IN'
+        elif self.token == 'OUT':
+            self.token.next()  # self.token >> 'OUT'
+        raise Exception  # todo
 
-def parameter_mode(lex):
-    # IN
-    # OUT
-    pass
+    def statement_list(self):
+        # <statement>
+        # <statement_list> <statement>
+        self.statement()
+        if self.token == 'INSERT' or self.token == 'SELECT':
+            self.statement_list()
 
+    def statement(self):
+        # <insert_statement>
+        # <select_statement>
+        if self.token == 'INSERT':
+            self.insert_statement()
+        elif self.token == 'SELECT':
+            self.select_statement()
+        raise Exception  # todo
 
-def statement_list(lex):
-    # <statement>
-    # <statement_list> <statement>
-    pass
+    def insert_statement(self):
+        # INSERT TABLE id VALUES LPARENT <argument_list> RPARENT SEMICOLON
+        self.token.next()  # self.token >> 'INSERT'
+        self.token >> 'TABLE'
+        table_name = self.token >> 'IDENTIFIER'
+        self.token >> 'VALUES'
+        self.token >> 'LPARENT'
+        self.argument_list()
+        self.token >> 'RPARENT'
+        self.token >> 'SEMICOLON'
 
+    def argument_list(self):
+        # <argument>
+        # <argument_list> COMMA <argument>
+        self.argument()
+        if self.token == 'COMMA':
+            self.token.next()  # self.token >> 'COMMA'
+            self.argument_list()
 
-def statement(lex):
-    # <insert_statement>
-    # <select_statement>
-    pass
+    def argument(self):
+        # pid
+        # CURRVAL LPARENT id RPARENT
+        # NEXTVAL LPARENT id RPARENT
+        if self.token == 'CURRVAL':
+            self.token.next()  # self.token >> 'CURRVAL'
+            self.token >> 'LPARENT'
+            sequence_name = self.token >> 'IDENTIFIER'
+            self.token >> 'RPARENT'
+            return
+        elif self.token == 'NEXTVAL':
+            self.token.next()  # self.token >> 'NEXTVAL'
+            self.token >> 'LPARENT'
+            sequence_name = self.token >> 'IDENTIFIER'
+            self.token >> 'RPARENT'
+            return
+        parameter_name = self.token >> 'PARAMETER'
 
+    def select_statement(self):
+        # SELECT <selection_list> FROM <table_list> WHERE <condition_list> SEMICOLON
+        self.token.next()  # self.token >> 'SELECT'
+        self.selection_list()
+        self.token >> 'FROM'
+        self.table_list()
+        self.token >> 'WHERE'
+        self.condition_list()
+        self.token >> 'SEMICOLON'
 
-def insert_statement(lex):
-    # INSERT TABLE id VALUES LPARENT <argument_list> RPARENT SEMICOLON
-    pass
+    def selection_list(self):
+        # <selection>
+        # <selection_list> COMMA <selection>
+        self.selection()
+        if self.token == 'COMMA':
+            self.token.next()  # self.token >> 'COMMA'
+            self.selection_list()
 
+    def selection(self):
+        # id SET pid
+        column_name = self.token >> 'IDENTIFIER'
+        self.token >> 'SET'
+        parameter_name = self.token >> 'PARAMETER'
 
-def argument_list(lex):
-    # <argument>
-    # <argument_list> COMMA <argument>
-    pass
+    def table_list(self):
+        # <table_list> JOIN id
+        # id
+        table_name = self.token >> 'IDENTIFIER'
+        if self.token == 'JOIN':
+            self.token.next()  # self.token >> 'JOIN'
+            self.table_list()
 
+    def condition_list(self):
+        # <search_cond_not>
+        # <search_cond_not> AND <condition_list>
+        # <search_cond_not> OR <condition_list>
+        # LPARENT <condition_list> RPARENT
+        # LPARENT <condition_list> RPARENT AND <condition_list>
+        # LPARENT <condition_list> RPARENT OR <condition_list>
+        if self.token == 'LPARENT':
+            self.token.next()  # self.token >> 'LPARENT'
+            self.condition_list()
+            self.token >> 'RPARENT'
+            if self.token == 'AND':
+                self.token.next()  # self.token >> 'AND'
+                self.condition_list()
+            elif self.token == 'OR':
+                self.token.next()  # self.token >> 'OR'
+                self.condition_list()
+            return
+        self.search_cond_not()
+        if self.token == 'AND':
+            self.token.next()  # self.token >> 'AND'
+            self.condition_list()
+        elif self.token == 'OR':
+            self.token.next()  # self.token >> 'OR'
+            self.condition_list()
 
-def argument(lex):
-    # pid
-    # CURRVAL LPARENT id RPARENT
-    # NEXTVAL LPARENT id RPARENT
-    pass
+    def search_cond_not(self):
+        # <condition_simple>
+        # NOT <condition_simple>
+        if self.token == 'NOT':
+            self.token.next()  # self.token >> 'NOT'
+        self.condition_list()
 
+    def condition_simple(self):
+        # id <operator> pid
+        # id <operator> id
+        pass
 
-def select_statement(lex):
-    # SELECT <selection_list> FROM <table_list> WHERE <condition_list> SEMICOLON
-    pass
+    def operator(self):
+        # EQ
+        # LESS
+        # MORE
+        # NOT_EQ
+        # LESS_OR_EQ
+        # MORE_OR_EQ
+        pass
 
-
-def selection_list(lex):
-    # <selection>
-    # <selection_list> COMMA <selection>
-    pass
-
-
-def selection(lex):
-    # id SET pid
-    pass
-
-
-def table_list(lex):
-    # <table_list> JOIN id
-    # id
-    pass
-
-
-def condition_list(lex):
-    # <search_cond_not>
-    # <search_cond_not> AND <condition_list>
-    # <search_cond_not> OR <condition_list>
-    # LPARENT <condition_list> RPARENT
-    # LPARENT <condition_list> RPARENT AND <condition_list>
-    # LPARENT <condition_list> RPARENT OR <condition_list>
-    pass
-
-
-def search_cond_not(lex):
-    # <condition_simple>
-    # NOT <condition_simple>
-    pass
-
-
-def condition_simple(lex):
-    # id <operator> pid
-    # id <operator> id
-    pass
-
-
-def operator(lex):
-    # EQ
-    # LESS
-    # MORE
-    # NOT_EQ
-    # LESS_OR_EQ
-    # MORE_OR_EQ
-    pass
-
-
-def sequence_declaration(lex):
-    # CREATE SEQUENCE id SEMICOLON
-    pass
+    def sequence_declaration(self):
+        # CREATE SEQUENCE id SEMICOLON
+        self.token >> 'SEQUENCE'
+        sequence_name = self.token >> 'IDENTIFIER'
+        self.token >> 'SEMICOLON'
