e754e7cfc67e90b3a277c0745834bf36807d1932
Denis <chugunov-official@mail.ru>
2020-02-02 20:18:28 +0300

    Minor changes

--- pymilldb/utils.py
+++ pymilldb/utils.py
@@ -1,18 +1,22 @@
 import logging
 
-size = 0
-step = 2
 
+# noinspection PyPep8Naming
+class log:
+    size = 0
+    step = 2
 
-def log(logger=logging, level=logging.DEBUG, name=None):
-    def decorator(func):
+    def __init__(self, logger=logging, level=logging.DEBUG, name=None):
+        self.logger = logger
+        self.level = level
+        self.name = name
+
+    def __call__(self, func):
         def new_func(*args, **kwargs):
-            global size
-            logger.log(level, '%s> %s', ' ' * size, func.__name__ if name is None else name)
-            size += step
+            self.logger.log(self.level, '%s> %s', ' ' * log.size, func.__name__ if self.name is None else self.name)
+            log.size += log.step
             out = func(*args, **kwargs)
-            size -= step
-            logger.log(level, '%s< %s', ' ' * size, func.__name__ if name is None else name)
+            log.size -= log.step
+            self.logger.log(self.level, '%s< %s', ' ' * log.size, func.__name__ if self.name is None else self.name)
             return out
         return new_func
-    return decorator
